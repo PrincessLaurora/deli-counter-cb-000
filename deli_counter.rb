dea9514dfffa540233416d4523a20fdb9515e6e4
Laura Felce <crepu83@yahoo.it>
2018-11-19 10:52:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.empty?
+        puts "The line is currently empty."
+  else
+    another_deli = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+  another_deli << " #{index + 1}. #{name}"
+    end
+    puts another_deli
+end
+end
